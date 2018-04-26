bb83abeae3f97fdd865fb32c5a973f70cb3aa488
Manjesh V <manjeshpv@gmail.com>
2018-04-26 18:41:42 +0530

    feat: db migration execute
    - added missing package handlebars

--- bin/emailq.js
+++ bin/emailq.js
@@ -5,3 +5,14 @@ const fs = require('fs')
 
 const index = path.join(path.dirname(fs.realpathSync(__filename)), '../server/index.js')
 const MailQ = require(index)
+const { exec } = require('child_process');
+exec('sequelize db:migrate', (err, stdout, stderr) => {
+  if (err) {
+    // node couldn't execute the command
+    return;
+  }
+
+  // the *entire* stdout and stderr (buffered)
+  console.log(`stdout: ${stdout}`);
+  console.log(`stderr: ${stderr}`);
+});
--- server/config/sequelize.js
+++ server/config/sequelize.js
@@ -7,12 +7,15 @@ const root = path.normalize(`${__dirname}/../..`);
 const envFile = path.join(root, '.env');
 let config = {};
 
+function getUserHome() {
+  return process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'];
+}
+
 if (fs.existsSync(envFile)) {
   const env = dotenv.config({ path: envFile });
   config = env.parsed || env;
 } else {
-  console.log(`.env file not found.
-  Please create manually or visit http://localhost:3000
+  console.log(`.emailq file not found.
   Learn more at check installation docs at https://github.com/manjeshpv/emailq/blob/${pkg.version}/docs/Installation.md
   Trying to connect with default settings.
   `);
@@ -20,7 +23,7 @@ if (fs.existsSync(envFile)) {
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 const settings = {
   dialect: "sqlite",
-  storage: process.env.DB_STORAGE || '../emailq.sqlite',
+  storage: process.env.DB_STORAGE || path.join(getUserHome(), 'emailq.sqlite'),
   seederStorage: 'sequelize',
 };
 
